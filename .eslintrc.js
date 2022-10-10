ca92b335e027d07f006c0fd2de0f4d2eb95725f2
yachae85 <yachae85@github.com>
2022-10-10 20:53:38 +0900

    init(2)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,14 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'no-unused-vars': 'warn',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        semi: [2, 'always'],
       },
     },
   ],
