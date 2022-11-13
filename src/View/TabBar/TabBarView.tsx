22cce65d4563fa38e4eecace569c5ba15ea048aa
yachae85 <yachae85@github.com>
2022-11-13 18:37:50 +0900

    tabbar 아이콘 이미지 추가

--- src/View/TabBar/TabBarView.tsx
+++ src/View/TabBar/TabBarView.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import {Button, View} from 'react-native';
+import React, {useState} from 'react';
+import {Image, View} from 'react-native';
 import styled from 'styled-components';
 import {createStackNavigator} from '@react-navigation/stack';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 
 import HomeView from '../Home/HomeView';
 import MyPageView from '../MyPage/MyPageView';
+
+const ACTIVE_HOME = '../../../assets/tb_home_active.png';
+const HOME = '../../../assets/tb_home.png';
+const ACTIVE_MYPAGE = '../../../assets/tb_mypage_active.png';
+const MYPAGE = '../../../assets/tb_mypage.png';
 
 type TabBarStackParamList = {
   TabBar: {screen: string};
@@ -15,14 +21,18 @@ type TabBarStackParamList = {
 const TabBar = createStackNavigator();
 
 const TabBarView: React.FC = () => {
+  const [isHome, setIsHome] = useState(true);
+
   const navigation = useNavigation<StackNavigationProp<TabBarStackParamList>>();
 
   const onHomeButtonClick = () => {
     navigation.navigate('TabBar', {screen: 'Home'});
+    setIsHome(true);
   };
 
   const onMyPageButtonClick = () => {
     navigation.navigate('TabBar', {screen: 'MyPage'});
+    setIsHome(false);
   };
 
   return (
@@ -36,8 +46,22 @@ const TabBarView: React.FC = () => {
           <TabBar.Screen name="MyPage" component={MyPageView} />
         </TabBar.Navigator>
       </NavigationWrapper>
-      <Button title="홈" onPress={onHomeButtonClick} />
-      <Button title="마이페이지" onPress={onMyPageButtonClick} />
+      <TabBarContainer>
+        <ImageContainer onPress={onHomeButtonClick}>
+          {isHome ? (
+            <TabBarImage source={require(ACTIVE_HOME)} />
+          ) : (
+            <TabBarImage source={require(HOME)} />
+          )}
+        </ImageContainer>
+        <ImageContainer onPress={onMyPageButtonClick}>
+          {isHome ? (
+            <TabBarImage source={require(MYPAGE)} />
+          ) : (
+            <TabBarImage source={require(ACTIVE_MYPAGE)} />
+          )}
+        </ImageContainer>
+      </TabBarContainer>
     </TabBarWrapper>
   );
 };
@@ -45,11 +69,40 @@ const TabBarView: React.FC = () => {
 const TabBarWrapper = styled(View)`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  background-color: white;
 `;
 
 const NavigationWrapper = styled(View)`
   width: 100%;
-  height: 80%;
+  flex: 1;
+`;
+
+const TabBarContainer = styled(View)`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  border-top-width: 0.2px;
+  border-style: solid;
+  border-color: #848488;
+`;
+
+const ImageContainer = styled(TouchableWithoutFeedback)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  height: 60px;
+`;
+
+const TabBarImage = styled(Image)`
+  width: 50px;
+  height: 50px;
 `;
 
 export default TabBarView;
