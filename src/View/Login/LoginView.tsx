7fd01ebfd658935274b60d98796a8dfe7f075774
yachae85 <yachae85@github.com>
2022-11-19 15:03:41 +0900

    feat: 로그인 페이지 디자인 1차 수정

--- src/View/Login/LoginView.tsx
+++ src/View/Login/LoginView.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
-import {Button, Text} from 'react-native';
+import {Button, Image, Text as _Text} from 'react-native';
+import {TouchableWithoutFeedback} from 'react-native-gesture-handler';
 import styled from 'styled-components';
 
 import {apiPost, apiGet} from '../../Api/api';
@@ -28,15 +29,51 @@ const LoginView: React.FC = ({navigation}: any) => {
 
   return (
     <LoginWrapper>
-      <Text>로그인</Text>
-      <Button title={'login'} onPress={onLoginButtonClick} />
+      <LogoImg source={require('../../../assets/default_logo.png')} />
+      <Text>
+        이 세상 <HiighlightText>참견쟁이</HiighlightText>들 모두 여기로 모여라!
+      </Text>
+      <KakaoLoginContaienr onPress={() => console.log('asd')}>
+        <Image
+          source={require('../../../assets/kakao_login_medium_wide.png')}
+        />
+      </KakaoLoginContaienr>
       <Button title={'aasdsd'} onPress={onButtonClick} />
     </LoginWrapper>
   );
 };
 
 const LoginWrapper = styled(Wrapper)`
-  background-color: ${props => props.theme.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
+
+const LogoImg = styled(Image)`
+  margin-bottom: 24px;
+`;
+
+const Text = styled(_Text)`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  color: #000;
+  opacity: 0.5;
+  margin-bottom: 55px;
+`;
+
+const HiighlightText = styled(Text)`
+  color: #f00000;
+`;
+
+const LoginImgContainer = styled(TouchableWithoutFeedback)`
+  height: 48px;
+`;
+
+const KakaoLoginContaienr = styled(LoginImgContainer)`
+  margin-bottom: 12px;
+`;
+
+const AppleLoginContainer = styled(LoginImgContainer)``;
 
 export default LoginView;
