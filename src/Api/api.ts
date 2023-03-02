82be8137af9daadad36891592f5aafb0b9d260f9
yachae85 <yachae85@github.com>
2023-03-02 16:29:19 +0900

    fix: api fix

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,27 +1,28 @@
 export default class Api {
   private static readonly baseURL =
-    'http://api-alpha.ap-northeast-2.elasticbeanstalk.com';
+    'http://api-chi.ap-northeast-2.elasticbeanstalk.com';
   private static readonly defaultHeader = {
     'Content-Type': 'application/json',
     Accept: '*/*',
   };
 
-  protected static apiGet(url: string, params?: any) {
+  protected static async apiGet(url: string, params?: any) {
     const requestUrl = params
       ? this.baseURL + url + '?' + new URLSearchParams(params)
       : this.baseURL + url;
-    console.log(requestUrl);
-    const response = fetch(requestUrl, {
+    const response = await fetch(requestUrl, {
       method: 'GET',
+      headers: this.defaultHeader,
     })
-      .then(res => res)
+      .then(res => res.json())
+      .then(data => data)
       .catch(e => console.error(e));
 
     return response;
   }
 
-  protected static apiPost(url: string, body: Object) {
-    const response = fetch(this.baseURL + url, {
+  protected static async apiPost(url: string, body: Object) {
+    const response = await fetch(this.baseURL + url, {
       method: 'POST',
       headers: this.defaultHeader,
       body: JSON.stringify(body),
